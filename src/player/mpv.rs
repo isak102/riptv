4fe59cbc9f4554f43528ea96306a8b2f8a66cab7
Isak <isak.drevstad10@gmail.com>
2023-03-30 21:21:08 +0200

    Add notif when selecting stream

--- src/player/mpv.rs
+++ src/player/mpv.rs
@@ -2,8 +2,8 @@ use std::process::{Command, ExitStatus};
 use url::Url;
 
 pub fn play(url: Url) -> ExitStatus {
-    
     // TODO: add channel name
+    super::super::notif::stream("PLACEHOLDER");
     let exit_status = Command::new("mpv")
         .arg("--load-unsafe-playlists")
         .arg("--loop-playlist=inf")
