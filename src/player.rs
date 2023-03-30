1176bf1e4ff6d7b6939f75d3043d8a3ea5babc59
Isak <isak.drevstad10@gmail.com>
2023-03-30 21:53:57 +0200

    Add Channel struct to hold title and url

--- src/player.rs
+++ src/player.rs
@@ -1,21 +1,28 @@
+mod connect_vpn;
 mod get_url;
 mod mpv;
-mod connect_vpn;
 
 use crate::StreamType;
 use std::path::PathBuf;
 use std::{error::Error, result::Result};
+use url::Url;
+
+#[derive(Debug)]
+struct Channel {
+    title: String,
+    url: Url,
+}
 
 pub fn play(
     stream_type: StreamType,
     data_directory: &PathBuf,
     _fzf: bool,
 ) -> Result<(), Box<dyn Error>> {
-    let result = get_url::dmenu(stream_type, data_directory)?;
+    let result = Channel::dmenu(stream_type, data_directory)?;
     eprintln!("{:?}", result);
 
-    if let Some(url) = result {
-        // FIXME: Check if stream is already playing and kill it. 
+    if let Some(Channel { title, url }) = result {
+        // FIXME: Check if stream is already playing and kill it.
         connect_vpn::connect("se-sto");
         mpv::play(url);
     } else {
--- src/player/get_url.rs
+++ src/player/get_url.rs
@@ -8,47 +8,72 @@ fn strip_non_ascii(s: &str) -> String {
     s.chars().filter(|c| c.is_ascii()).collect()
 }
 
-pub(super) fn dmenu(
-    stream_type: StreamType,
-    data_directory: &PathBuf,
-) -> Result<Option<Url>, Box<dyn Error>> {
-    let prompt = "Live channel📺";
+fn get_channel_title(channels_file: &PathBuf, url: &Url) -> String {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "grep \"{}\" \"{}\" | awk -F§ 'NF{{NF-=1}};1'",
+            url,
+            channels_file.to_str().unwrap()
+        ))
+        .output()
+        .expect("Failed to execute command");
 
-    let channels = Command::new("cat")
-        .arg(data_directory.join(format!("{stream_type}.txt")))
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to execute cat command");
+    let title_untrimmed = String::from_utf8(output.stdout).unwrap(); // TODO: remove unwrap
+    let title = title_untrimmed.trim();
 
-    let dmenu_command = Command::new("dmenu")
-        .arg("-D")
-        .arg("§")
-        .arg("-l")
-        .arg("20")
-        .arg("-g")
-        .arg("2")
-        .arg("-p")
-        .arg(prompt)
-        .arg("-vf")
-        .stdin(channels.stdout.unwrap())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to execute dmenu command");
+    println!("{}", title);
+    title.to_string()
+}
 
-    let dmenu_output = dmenu_command
-        .wait_with_output()
-        .expect("failed to read dmenu output")
-        .stdout;
-    let selection: String = String::from_utf8_lossy(&dmenu_output).into_owned();
+impl Channel {
+    pub fn dmenu(
+        stream_type: StreamType,
+        data_directory: &PathBuf,
+    ) -> Result<Option<Channel>, Box<dyn Error>> {
+        let prompt = "Live channel📺"; // TODO: fix
 
-    let unstripped_url =
-        std::str::from_utf8(&strip_ansi_escapes::strip(selection.as_str()).unwrap())
-            .unwrap()
-            .to_string();
-    let url = strip_non_ascii(unstripped_url.trim());
+        let channels_file = data_directory.join(format!("{stream_type}.txt"));
+        let channels = Command::new("cat")
+            .arg(&channels_file)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute cat command");
 
-    match Url::parse(url.as_str()) {
-        Ok(url_option) => Ok(Some(url_option)),
-        Err(_) => Ok(None),
+        let dmenu_command = Command::new("dmenu")
+            .arg("-D")
+            .arg("§")
+            .arg("-l")
+            .arg("20")
+            .arg("-g")
+            .arg("2")
+            .arg("-p")
+            .arg(prompt)
+            .arg("-vf")
+            .stdin(channels.stdout.unwrap())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute dmenu command");
+
+        let dmenu_output = dmenu_command
+            .wait_with_output()
+            .expect("failed to read dmenu output")
+            .stdout;
+        let selection: String = String::from_utf8_lossy(&dmenu_output).into_owned();
+
+        let unstripped_url =
+            std::str::from_utf8(&strip_ansi_escapes::strip(selection.as_str()).unwrap())
+                .unwrap()
+                .to_string();
+        let stipped_url = strip_non_ascii(unstripped_url.trim());
+
+        let url = match Url::parse(stipped_url.as_str()) {
+            Ok(url_option) => url_option,
+            Err(_) => return Ok(None),
+        };
+
+        let title = get_channel_title(&channels_file, &url);
+
+        Ok(Some(Channel { url, title }))
     }
 }
